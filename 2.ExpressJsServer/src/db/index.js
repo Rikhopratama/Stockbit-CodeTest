562090eb8a9b262e0584a65c429e7f5ae5fccc03
Rikho Pratama <rikhopratama@PTs-MacBook-Pro.local>
2021-04-03 12:37:33 +0700

    Move credentials to environment

--- 2.ExpressJsServer/src/db/index.js
+++ 2.ExpressJsServer/src/db/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('movie_search', 'root', null, {
-  host: '127.0.0.1',
-  dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_URL,
+  dialect: process.env.DB_DIALECT
 });
 
 sequelize.authenticate().then(() => {
